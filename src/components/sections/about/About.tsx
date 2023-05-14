60d86e562a649dde9a2884cd4a5284ec26d25db2
DeibydB <deibydbarragan@gmail.com>
2023-05-13 23:45:06 -0500

    Arreglé ciertos divs innecesarios

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -4,24 +4,22 @@ import { cardsData } from "./cards.data"
 export default function About() {
   return (
     <section
-      className="pt-16 section"
+      className="pt-16 section w-full sm:w-10/12  md:w-7/12 lg:w-6/12 2xl:w-5/12"
       id="about"
     >
-      <div className="flex flex-col md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-4/12 gap-6">
-        <h2>Sobre mí</h2>
-        <p>
-          I am a full-stack web developer with experience in React, JavaScript, HTML, CSS, Tailwind, Bootstrap, Laravel, and relational database management with SQL, as well as Agile methodologies. I am capable of developing and maintaining complex web applications using a combination of cutting-edge technologies and proven development practices. Additionally, I am passionate about learning and constantly improving, always seeking to update my skills and expand my knowledge in the field of web development. My strong communication skills and problem-solving abilities make me a valuable team member, able to effectively communicate technical concepts and find creative solutions to challenges.
-        </p>
-        <div className="grid grid-cols-1  sm:grid-cols-2 gap-3">
-          {
-            cardsData.map((card, index) => (
-              <NeonCard
-                key={index}
-                {...card}
-              />
-            ))
-          }
-        </div>
+      <h2>Sobre mí</h2>
+      <p>
+        I am a full-stack web developer with experience in React, JavaScript, HTML, CSS, Tailwind, Bootstrap, Laravel, and relational database management with SQL, as well as Agile methodologies. I am capable of developing and maintaining complex web applications using a combination of cutting-edge technologies and proven development practices. Additionally, I am passionate about learning and constantly improving, always seeking to update my skills and expand my knowledge in the field of web development. My strong communication skills and problem-solving abilities make me a valuable team member, able to effectively communicate technical concepts and find creative solutions to challenges.
+      </p>
+      <div className="grid grid-cols-1  sm:grid-cols-2 gap-3">
+        {
+          cardsData.map((card, index) => (
+            <NeonCard
+              key={index}
+              {...card}
+            />
+          ))
+        }
       </div>
     </section>
   )
--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -4,39 +4,37 @@ import { Link } from "react-scroll"
 
 export default function Hero() {
   return (
-    <main className="flex align-items-center min-h-screen mt-10 sm:mt-0">
-      <div className="mx-auto flex flex-col md:flex-row justify-center items-center w-auto md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 gap-6">
-        <Image
-          src="/images/foto.png"
-          alt="Picture of the author"
-          width={250}
-          height={250}
-          className="rounded-full"
+    <main className="section mx-auto md:flex-row items-center w-7/12 2xl:w-6/12">
+      <Image
+        src="/images/foto.png"
+        alt="Picture of the author"
+        width={250}
+        height={250}
+        className="rounded-full"
 
-        />
-        <div className="flex flex-col gap-3">
-          <h3>Debyd Barragán</h3>
-          <h1>Desarrollador fullstack</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Eget suspendisse suspendisse dui scelerisque at. Sit et in urna enim tincidunt eu massa cras. Elementum facilisi at faucibus nunc. Accumsan placerat rhoncus nulla volutpat pellentesque laoreet. 
-          </p>
-          <div className="flex gap-3">
-            <a
-              href="/files/CV.pdf" 
-              download="CV.pdf"
-              className="btn bg-gradient-to-r from-indigo-700 to-orange-500"  
-            >
-              Descargar CV
-              <HiDocumentDownload className="text-xl"/>
-            </a>
-            <Link
-              className="btn bg-gradient-to-r from-indigo-700 to-orange-500"
-              to="contact"
-            >
-              Contáctame
-              <HiMail className="text-xl"/>
-            </Link>
-          </div>
+      />
+      <div className="flex flex-col gap-3">
+        <h3>Debyd Barragán</h3>
+        <h1>Desarrollador fullstack</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Eget suspendisse suspendisse dui scelerisque at. Sit et in urna enim tincidunt eu massa cras. Elementum facilisi at faucibus nunc. Accumsan placerat rhoncus nulla volutpat pellentesque laoreet. 
+        </p>
+        <div className="flex gap-3">
+          <a
+            href="/files/CV.pdf" 
+            download="CV.pdf"
+            className="btn bg-gradient-to-r from-indigo-700 to-orange-500"  
+          >
+            Descargar CV
+            <HiDocumentDownload className="text-xl"/>
+          </a>
+          <Link
+            className="btn bg-gradient-to-r from-indigo-700 to-orange-500"
+            to="contact"
+          >
+            Contáctame
+            <HiMail className="text-xl"/>
+          </Link>
         </div>
       </div>
     </main>
