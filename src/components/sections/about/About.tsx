5db1b01ffbedab66ee55362cb3dfc3379531be74
DeibydB <deibydbarragan@gmail.com>
2023-05-27 22:17:48 -0500

    Sección de educación con animaciones

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -10,15 +10,6 @@ export default function About() {
       className="sm:w-10/12  md:w-7/12 lg:w-6/12 2xl:w-5/12"
       id="about"
     >
-      <BgShapes
-        className="bg-orange-600 w-60 h-60 bottom-20 animate-blob"
-      />
-      <BgShapes
-        className="bg-indigo-600 w-60 h-60 left-40 bottom-20 animate-blob animation-delay-2"
-      />
-      <BgShapes
-        className="bg-sky-600 w-60 h-60 left-96 bottom-20 animate-blob animation-delay-3"
-      />
       <motion.div
         className="flex flex-col gap-3"
         initial={{ opacity: 0, x: -10 }}
@@ -29,7 +20,16 @@ export default function About() {
           I am a full-stack web developer with experience in React, JavaScript, HTML, CSS, Tailwind, Bootstrap, Laravel, and relational database management with SQL, as well as Agile methodologies. I am capable of developing and maintaining complex web applications using a combination of cutting-edge technologies and proven development practices. Additionally, I am passionate about learning and constantly improving, always seeking to update my skills and expand my knowledge in the field of web development. My strong communication skills and problem-solving abilities make me a valuable team member, able to effectively communicate technical concepts and find creative solutions to challenges.
         </p>
       </motion.div>
-      <div className="grid grid-cols-1  sm:grid-cols-2 gap-3">
+      <div className="relative grid grid-cols-1  sm:grid-cols-2 gap-3">
+        <BgShapes
+          className="bg-orange-600 w-60 h-60 animate-blob"
+        />
+        <BgShapes
+          className="bg-indigo-600 w-60 h-60 left-40 animate-blob animation-delay-2"
+        />
+        <BgShapes
+          className="bg-sky-600 w-60 h-60 left-96 animate-blob animation-delay-3"
+        />
         {
           cardsData.map((card, index) => (
             <NeonCard
--- src/components/sections/education/Education.tsx
+++ src/components/sections/education/Education.tsx
@@ -1,35 +1,82 @@
 import React from 'react'
-import { HiCode } from 'react-icons/hi'
+import { AiFillRightCircle } from 'react-icons/ai'
 import Section from '../Section'
+import { motion } from 'framer-motion';
+import BgShapes from '@/components/pure/bgShapes/BgShapes';
 
 type Props = {}
 
 export default function Education({}: Props) {
+  const liAnimation = {
+    initial: {
+      opacity: 0,
+      x: -10
+    },
+    whileInView: {
+      opacity: 1,
+      x: 0
+    },
+  }
+
   return (
     <Section
-      className='bg-red-200 section'
+      className='sm:w-10/12  md:w-8/12 lg:w-6/12 2xl:w-5/12'
       id='education'
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
     >
-      <div className='flex flex-col md:w-8/12 lg:w-7/12 xl:w-6/12 2xl:w-4/12 gap-6'>
-        <h2>Educación</h2>
-          <ul className='list-inside border-l-2 pl-4 border-solid border-gradient shadow-left relative'>
-            <li className='flex items-center'>
-              <HiCode className='text-2xl bg-white dark:bg-black absolute'/>
-              <p>
-                Técnico en programación de software en formación(3er trimestre)
-                <br/>Servicio Nacional de Aprendizaje (SENA)
-                <br/>Centro de Materiales y Ensayos
-                <br/>En proceso de certificación
-              </p>
-            </li>
-            <li>
+      <BgShapes
+        className='h-56 w-56 bottom-20 left-0 bg-gradient-to-tr from-red-500 to-indigo-700'
+      />
+      <BgShapes
+        className='h-72 w-72 top-30 right-0 bg-orange-500'
+      />
+      <BgShapes
+        className='h-72 w-72 top-0 left-0 bg-sky-500'
+      />
+      <h2>Educación</h2>
+        <div className='relative flex flex-col gap-3 rounded-lg bg-white/70 dark:bg-zinc-950/70 p-6'>
+          <motion.div
+            className='flex items-center gap-3'
+            transition={{delay: 0.3}}
+            {...liAnimation}
+          >
+            <AiFillRightCircle className="text-3xl text-indigo-700"/>
+            <p>
               Técnico en programación de software en formación(3er trimestre)
               <br/>Servicio Nacional de Aprendizaje (SENA)
-              <br/>Centro de Materiales y Ensayos
-            </li>
-          </ul>
-        <h2>Experiencia</h2>
-      </div>
+              <br/>En proceso de certificación
+            </p>
+          </motion.div>
+          <hr/>
+          <motion.div
+            className='flex items-center gap-3'
+            transition={{delay: 0.6}}
+            {...liAnimation}
+          >
+            <AiFillRightCircle className="text-3xl text-indigo-700"/>
+            <p>
+              Bachiller técnico con énfasis en gestión de proyectos culturales
+              <br/>Colegio El Porvenir IED (Bogotá D.C)
+              <br/>2020
+            </p>
+          </motion.div>
+        </div>
+      <h2>Experiencia</h2>
+      <div className='relative flex flex-col gap-3 divide-y rounded-lg bg-white/70 dark:bg-zinc-950/70 p-6'>
+          <motion.div
+            className='flex items-center gap-3'
+            transition={{delay: 0.9}}
+            {...liAnimation}
+          >
+            <AiFillRightCircle className="text-3xl text-indigo-700"/>
+            <p>
+              Técnico en programación de software en formación(3er trimestre)
+              <br/>Servicio Nacional de Aprendizaje (SENA)
+              <br/>En proceso de certificación
+            </p>
+          </motion.div>
+        </div>
     </Section>
   )
 }
