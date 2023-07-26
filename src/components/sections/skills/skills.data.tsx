9d3c27cb82ac679b6854e285cd13d3b9f7be3e47
DeibydB <deibydbarragan@gmail.com>
2023-07-26 01:32:29 -0500

    Actualización de skills

--- src/components/sections/skills/skills.data.tsx
+++ src/components/sections/skills/skills.data.tsx
@@ -1,9 +1,10 @@
-import { SiJavascript, SiTypescript, SiCss3, SiHtml5, SiPhp, SiNodedotjs, SiExpress, SiLaravel, SiMysql, SiFirebase, SiReact, SiTailwindcss,SiBootstrap, SiNextdotjs, SiChakraui, SiGit, SiGithub, SiFigma, SiNpm, SiVisualstudiocode, SiNotion, SiDaisyui} from "react-icons/si"
+import { SiJavascript, SiDocker, SiTypescript, SiPostgresql, SiPostman, SiMongodb, SiCss3, SiSpringboot, SiHtml5, SiPhp, SiExpress, SiLaravel, SiMysql, SiFirebase, SiReact, SiTailwindcss,SiBootstrap, SiNextdotjs, SiChakraui, SiGit, SiGithub, SiFigma, SiNpm, SiVisualstudiocode, SiNotion, SiDaisyui} from "react-icons/si"
 
 import { HiCode } from "react-icons/hi";
 import { HiServerStack, HiCube } from "react-icons/hi2";
 import { BiCodeCurly } from "react-icons/bi";
 import { Categorie } from "./skillCard/interfaces";
+import {FaJava} from "react-icons/fa";
 
 export const categories: Categorie[] = [
   {
@@ -25,6 +26,13 @@ export const categories: Categorie[] = [
         version: 'ES6'
       },
       {
+        name: 'Java',
+        color: 'text-red-500',
+        icon: <FaJava/>,
+        level: 'Fundamentos',
+        version: '17'
+      },
+      {
         name: 'HTML',
         color: 'text-red-500',
         icon: <SiHtml5/>,
@@ -42,7 +50,7 @@ export const categories: Categorie[] = [
         name: 'PHP',
         color: 'text-blue-600',
         icon: <SiPhp/>,
-        level: 'Básico',
+        level: 'Fundamentos',
         version: '7'
       }
     ]
@@ -52,24 +60,52 @@ export const categories: Categorie[] = [
     icon: <HiServerStack/>,
     skills: [
       {
+        name: 'Spring Boot',
+        color: 'text-green-500',
+        icon: <SiSpringboot/>,
+        level: 'Fundamentos',
+        version: '3.1.2'
+      },
+      {
+        name: 'Express',
+        color: 'text-white',
+        icon: <SiExpress/>,
+        level: 'Fundamentos',
+        version: '4.18'
+      },
+      {
         name: 'Laravel',
         color: 'text-red-500',
         icon: <SiLaravel/>,
-        level: 'Intermedio',
+        level: 'Fundamentos',
         version: '8'
       },
       {
         name: 'Firebase',
         color: 'text-yellow-500',
         icon: <SiFirebase/>,
-        level: 'Intermedio',
+        level: 'Fundamentos',
         version: '9.6'
+      },
+      {
+        name: 'MongoDB',
+        color: 'text-green-500',
+        icon: <SiMongodb/>,
+        level: 'Fundamentos',
+        version: '5'
+      },
+      {
+        name: 'PostgreSQL',
+        color: 'text-blue-500',
+        icon: <SiPostgresql/>,
+        level: 'Fundamentos',
+        version: '15'
       },
       {
         name: 'MySQL',
         color: 'text-blue-500',
         icon: <SiMysql/>,
-        level: 'Básico',
+        level: 'Fundamentos',
         version: '8'
       },  
     ]
@@ -82,21 +118,21 @@ export const categories: Categorie[] = [
         name: 'React',
         color: 'text-blue-500',
         icon: <SiReact/>,
-        level: 'Avanzado',
+        level: 'Intermedio',
         version: '16'
       },
       {
         name: 'Tailwind CSS',
         color: 'text-sky-500',
         icon: <SiTailwindcss/>,
-        level: 'Avanzado',
+        level: 'Intermedio',
         version: '3'
       },
       {
         name: 'NextJS',
         color: 'text-black dark:text-white',
         icon: <SiNextdotjs/>,
-        level: 'Avanzado',
+        level: 'Intermedio',
         version: '13'
       },
       {
@@ -109,13 +145,19 @@ export const categories: Categorie[] = [
         name: 'DaisyUI',
         color: '',
         icon: <SiDaisyui/>,
-        level: 'Básico'
+        level: 'Fundamentos'
+      },
+      {
+        name: 'NextUI',
+        color: '',
+        icon: <div className='flex justify-center items-center bg-black p-1 rounded-xl font-light text-5xl text-white w-[80px] h-full'> UI </div>,
+        level: 'Fundamentos'
       },
       {
         name: 'Bootstrap',
         color: 'text-purple-500',
         icon: <SiBootstrap/>,
-        level: 'Básico',
+        level: 'Fundamentos',
         version: '8'
       },
     ]
@@ -124,6 +166,18 @@ export const categories: Categorie[] = [
     name: 'Herramientas',
     icon: <HiCube/>,
     skills: [
+      {
+        name: 'Postman',
+        color: 'text-orange-600',
+        icon: <SiPostman/>,
+        level: 'Intermedio',
+      },
+      {
+        name: 'Docker',
+        color: 'text-blue-500',
+        icon: <SiDocker/>,
+        level: 'Fundamentos',
+      },
       {
         name: 'Git',
         color: 'text-red-500',
@@ -141,7 +195,7 @@ export const categories: Categorie[] = [
         name: 'Figma',
         color: 'text-purple-500',
         icon: <SiFigma/>,
-        level: 'Básico',
+        level: 'Fundamentos',
         version: '7'
       },
       {
